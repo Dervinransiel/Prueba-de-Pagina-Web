0ab0f3de6f7897d17c64150472fd5aa731c59b50
Dervinransiel <78989353+Dervinransiel@users.noreply.github.com>
2022-08-28 00:01:25 -0400

    Add files via upload

---
+++ HTML Practica Final/js/app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTAcvrfuNnUVapqnFXRX1xxr8RG5RlY-U",
+    authDomain: "crud-a720c.firebaseapp.com",
+    projectId: "crud-a720c",
+    storageBucket: "crud-a720c.appspot.com",
+    messagingSenderId: "557287516507",
+    appId: "1:557287516507:web:9000170a7519e8cb47927e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
